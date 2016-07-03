29e08574bacf3746f99cd655db1766f369773ba2
fang930142072 <930142072@qq.com>
2016-07-04 01:19:29 +0800

    首页响应式完成-end

--- js/cssChange.js
+++ js/cssChange.js
@@ -4,18 +4,24 @@
 
 //窗口变化事件
 function windowResize() {
-    if($("html").height()>410){
-        $(".container-ul").css('marginTop',($("html").height()-410)/2);
+    var hei = $(".container-minbox").height()*3+20;
+    $(".screen li").width($(".maxbox-bott").width());
+    if($("html").height()>hei){
+        $(".container-ul").css('marginTop',($("html").height()-hei-20)/2);
     }else{
-        $("html").height(430);
+        $("html").height(hei+20);
     }
 }
 $(function () {
     //给轮播图部分 li设置宽高
-
     $(".screen li").width($(".maxbox-bott").width());
+    $(".maxbox-bott").resize(function () {
+        console.log("0")
+
+    })
 
     windowResize();
+
     $(window)[0].onresize =windowResize;//监控浏览器窗口变化
 })
 
